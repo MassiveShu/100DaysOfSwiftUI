a398282039c2d1ba9714c9f9e3ffe51815fb2a5a
MassiveShu <shulyakmaksim@gmail.com>
2022-07-09 14:39:09 +0300

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Max Shu on 08.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
