18346c07ba37a762729be19506328c537514a0b0
MassiveShu <shulyakmaksim@gmail.com>
2022-07-09 14:45:04 +0300

    Add files via upload

---
+++ WeSplitChallengeDay18/WeSplitChallengeDay18/WeSplitChallengeDay18App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitChallengeDay18App.swift
+//  WeSplitChallengeDay18
+//
+//  Created by Max Shu on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitChallengeDay18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
