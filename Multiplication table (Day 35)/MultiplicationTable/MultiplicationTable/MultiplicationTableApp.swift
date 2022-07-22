40ff2ccdffe79c11e41872058da72a7380482f27
MassiveShu <shulyakmaksim@gmail.com>
2022-07-22 16:18:03 +0300

    Multiplication table (Day 35)

---
+++ Multiplication table (Day 35)/MultiplicationTable/MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Max Shu on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Multiplication table (Day 35)/MultiplicationTable/MultiplicationTable/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  MultiplicationTable
+//
+//  Created by Max Shu on 22.07.2022.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let correctAnswer: Int
+    let answers: [String]
+}
