e39c6298bb14b4d81318f36174fa5dcd110f7198
MassiveShu <shulyakmaksim@gmail.com>
2022-07-28 19:25:17 +0300

    Project 9 (Day 43-46) Drawing

---
+++ Project 9 (Day 43-46) Drawing/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Max Shu on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
